5f84d8d7f09b9b5087cdedd76c89f8d4ed6712ba
Paul Sladek <pbakersladek@gmail.com>
2021-03-28 12:51:49 -0700

    init

--- test/snetc_test.clj
+++ test/snetc_test.clj
@@ -3,14 +3,14 @@
                  [snetc :refer [inet-aton, inet-ntoa, network-address]]))
 
 (def network "192.168.0.0")
-(def network-int "-1062731776")
+(def network-int -1062731776)
 (def mask 22)
 
-(deftest inet-ntoa
+(deftest inet-aton-test
   (is (= network-int (inet-aton network))))
 
-(deftest inet-ntoa
+(deftest inet-ntoa-test
   (is (= network (inet-ntoa network-int))))
 
-(deftest network-address
+(deftest network-address-test
   (is (= network-int (network-address network-int mask))))
