13ad5db0a938e0dd2431ada2dd67b6b5d19a1c97
Paul Sladek <pbakersladek@gmail.com>
2021-04-11 16:50:46 -0700

    init

--- src/snetc/snetc.clj
+++ src/snetc/snetc.clj
@@ -1,28 +1,34 @@
 (ns snetc
   (:require [clojure.tools.cli :refer [parse-opts]]
-            [clojure.string :as string])
+            [clojure.string :as string]
+            [clojure.pprint :refer [print-table]])
   (:gen-class))
-
-(defn inet-ntoa [addrint]
-  (let [first (bit-and (clojure.lang.Numbers/shiftRightInt addrint 24) 0xff)
-        second (bit-and (clojure.lang.Numbers/shiftRightInt addrint 16) 0xff)
-        third (bit-and (clojure.lang.Numbers/shiftRightInt addrint 8) 0xff)
-        fourth (bit-and addrint 0xff)]
-    (str first "." second "." third "." fourth)))
 
 (defn parse-int-nth [match, index]
   (Integer/parseInt (nth match index)))
 
-(defn inet-aton [addrstr]
+(defn inet-ntoa
+  "Converts the Internet host address in, given in network byte order, to a string in IPv4 dotted-decimal notation"
+  [in]
+  (let [first (bit-and (clojure.lang.Numbers/shiftRightInt in 24) 0xff)
+        second (bit-and (clojure.lang.Numbers/shiftRightInt in 16) 0xff)
+        third (bit-and (clojure.lang.Numbers/shiftRightInt in 8) 0xff)
+        fourth (bit-and in 0xff)]
+    (str first "." second "." third "." fourth)))
+
+(defn inet-aton
+  "Converts the Internet host address ha from the IPv4 numbers-and-dots notation into binary form (in network byte order)"
+  [ha]
   (let [cidr-format #"^([0-9]{1,3})\.([0-9]{1,3})\.([0-9]{1,3})\.([0-9]{1,3})$"
-        match (string/split (first (re-matches cidr-format addrstr)) #"\.")
+        match (string/split (first (re-matches cidr-format ha)) #"\.")
         first (clojure.lang.Numbers/shiftLeftInt (parse-int-nth match 0) 24)
         second (clojure.lang.Numbers/shiftLeftInt (parse-int-nth match 1) 16)
         third (clojure.lang.Numbers/shiftLeftInt (parse-int-nth match 2) 8)
         fourth (parse-int-nth match 3)]
     (bit-or (bit-or (bit-or first second) third) fourth)))
 
-(defn network-address [ip, mask]
+(defn network-address
+  [ip, mask]
   (loop [i (- 31 mask)
          ipa  ip]
     (if (zero? i)
@@ -40,6 +46,12 @@
 (defn subnet-netmask [mask]
   (network-address -1 mask))
 
+(defn hosts [useable-first useable-last mask]
+  (cond
+    (= mask 32) "1"
+    (>= mask 31) "2"
+    :else ((+ (- useable-last useable-first) 1))))
+
 (defn useable-range [network, mask]
   (let [aton (inet-aton network)
         net (network-address aton mask)
@@ -52,20 +64,24 @@
 (defn full-range [network, mask]
   (let [aton (inet-aton network)
         net (network-address aton mask)
-        net-last-address (subnet-last-address aton mask)
-        range (str (inet-ntoa net) " - " (inet-ntoa net-last-address))]
-    range))
+        net-last-address (subnet-last-address aton mask)]
+    (str (inet-ntoa net) " - " (inet-ntoa net-last-address))))
 
-(defn table [options]
+(defn gen-table-data [net, mask, useable-range, full-range]
+  [{:network (inet-ntoa net)
+    :mask mask
+    :useable-range useable-range
+    :full-range full-range
+    :hosts (hosts useable-first useable-last mask)}]))
+
+(defn gen [options]
   (let [network (get options :network)
-        verbosity (get options :verbosity)
         mask (get options :mask)
         aton (inet-aton network)
         net (network-address aton mask)
         useable-range (useable-range network mask)
         full-range (full-range network mask)]
-    (println "range: " useable-range)
-    (println "full range: " full-range)))
+    (print-table (gen-table-data net mask useable-range full-range))))
 
 (def cli-options
   [["-n" "--network 192.168.0.0" "Network address"
@@ -73,10 +89,10 @@
    ["-m" "--mask 22" "Mask"
     :parse-fn #(Integer/parseInt %)
     :validate [#(< 0 % 33) "The network mask you have entered is invalid. Must be 0 < 33."]]
-   ["-v" nil "Verbosity level"
-    :id :verbosity
-    :default 0
-    :update-fn inc]
+  ;;  ["-v" nil "Verbosity level"
+  ;;   :id :verbosity
+  ;;   :default 0
+  ;;   :update-fn inc]
    ["-h" "--help"]])
 
 (defn usage [options-summary]
@@ -123,4 +139,4 @@
     (if exit-message
       (exit (if ok? 0 1) exit-message)
       (case action
-        "gen"  (table options)))))
+        "gen"  (gen options)))))
